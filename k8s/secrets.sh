c33f96b275a64c51cd0763e3531690ce0aec74b2
xGANDAx <jonathanchitivam@gmail.com>
2026-04-19 17:28:57 -0500

    Added secrets step

---
+++ k8s/secrets.sh
@@ -0,0 +1,6 @@
+kubectl create secret generic -n backstage google-secrets \
+  --from-literal=AUTH_GOOGLE_CLIENT_ID='$AUTH_GOOGLE_CLIENT_ID' \
+  --from-literal=AUTH_GOOGLE_CLIENT_SECRET='$AUTH_GOOGLE_CLIENT_SECRET' \
+  --from-literal=GCS_CLIENT_EMAIL='$GCS_CLIENT_EMAIL' \
+  --from-literal=GCS_PRIVATE_KEY='$GCS_PRIVATE_KEY'
+
