7e943c13f4519673c41861bec124612b103a70f2
xGANDAx <jonathanchitivam@gmail.com>
2026-04-19 20:23:28 -0500

    Fix auth

--- k8s/secrets.sh
+++ k8s/secrets.sh
@@ -1,8 +1,8 @@
 kubectl delete secret google-secrets -n backstage
 
 kubectl create secret generic -n backstage google-secrets \
-  --from-literal=AUTH_GOOGLE_CLIENT_ID='$AUTH_GOOGLE_CLIENT_ID' \
-  --from-literal=AUTH_GOOGLE_CLIENT_SECRET='$AUTH_GOOGLE_CLIENT_SECRET' \
-  --from-literal=GCS_CLIENT_EMAIL='$GCS_CLIENT_EMAIL' \
-  --from-literal=GCS_PRIVATE_KEY='$GCS_PRIVATE_KEY'
+  --from-literal=AUTH_GOOGLE_CLIENT_ID="$AUTH_GOOGLE_CLIENT_ID" \
+  --from-literal=AUTH_GOOGLE_CLIENT_SECRET="$AUTH_GOOGLE_CLIENT_SECRET" \
+  --from-literal=GCS_CLIENT_EMAIL="$GCS_CLIENT_EMAIL" \
+  --from-literal=GCS_PRIVATE_KEY="$GCS_PRIVATE_KEY"
 
